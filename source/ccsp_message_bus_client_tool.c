06b1b98f8dea7c32a0429ce80e313f81bd1e6213
Andre McCurdy <armccurdy@gmail.com>
2021-01-06 12:51:52 -0800

    add dmcli support for Alias Manager
    Tests done:
    
    1: dmcli eRT getv Device.NAT.X_LGI-COM_DMZ.
    
      CR component name is: eRT.com.cisco.spvtg.ccsp.CR
      subsystem_prefix eRT.
      getv from/to component(eRT.com.cisco.spvtg.ccsp.pam): Device.NAT.X_CISCO_COM_DMZ.
      Execution succeed.
      Parameter    1 name: Device.NAT.X_CISCO_COM_DMZ.Enable
                     type:       bool,    value: false
      Parameter    2 name: Device.NAT.X_CISCO_COM_DMZ.RemoteIPStart
                     type:     string,    value:
      Parameter    3 name: Device.NAT.X_CISCO_COM_DMZ.RemoteIPEnd
                     type:     string,    value:
      Parameter    4 name: Device.NAT.X_CISCO_COM_DMZ.InternalIP
                     type:     string,    value: 192.168.0.0
      Parameter    5 name: Device.NAT.X_CISCO_COM_DMZ.InternalMAC
                     type:     string,    value:
      Parameter    6 name: Device.NAT.X_CISCO_COM_DMZ.IPv6Host
                     type:     string,    value:
      Parameter    7 name: Device.NAT.X_CISCO_COM_DMZ.Data
                     type:     string,    value:
    
    2: dmcli eRT getv Device.X_LGI-COM_General.CustomerId
    
      CR component name is: eRT.com.cisco.spvtg.ccsp.CR
      subsystem_prefix eRT.
      getv from/to component(eRT.com.cisco.spvtg.ccsp.pam): Device.X_LGI-COM_General_Internal.CustomerId
      Execution succeed.
      Parameter    1 name: Device.X_LGI-COM_General_Internal.CustomerId
                     type:       uint,    value: 0
    
    3: dmcli eRT setattributes  Device.NAT.X_CISCO_COM_DMZ.Enable active acs Device.UPnP.Device.X_LGI-COM_IGD_TTL passive acs
    
      CR component name is: eRT.com.cisco.spvtg.ccsp.CR
      subsystem_prefix eRT.
      setattributes from/to component(eRT.com.cisco.spvtg.ccsp.pam): Device.NAT.X_CISCO_COM_DMZ.Enable
      Execution succeed.
    
    4: dmcli eRT getattributes  Device.NAT.X_CISCO_COM_DMZ.Enable Device.UPnP.Device.X_LGI-COM_IGD_TTL
    
      CR component name is: eRT.com.cisco.spvtg.ccsp.CR
      subsystem_prefix eRT.
      getattributes from/to component(eRT.com.cisco.spvtg.ccsp.pam): Device.NAT.X_CISCO_COM_DMZ.Enable
      Execution succeed.
      Parameter    1 name: Device.NAT.X_CISCO_COM_DMZ.Enable
              notification:on, accessControlChanged:acs.
      Parameter    2 name: Device.UPnP.Device.X_CISCO_COM_IGD_TTL
              notification:on, accessControlChanged:acs.
    
    5: dmcli eRT setv Device.Users.User.3.Password string "password"
    
      CR component name is: eRT.com.cisco.spvtg.ccsp.CR
      subsystem_prefix eRT.
      setv from/to component(eRT.com.cisco.spvtg.ccsp.pam): Device.Users.User.3.X_CISCO_COM_Password
      Execution succeed.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    update path to Alias Manager mapper file
    
    The Alias Manager mapper file is not specific to TR069.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/ccsp_message_bus_client_tool.c
+++ source/ccsp_message_bus_client_tool.c
@@ -44,6 +44,8 @@
 #include <signal.h>
 #include "ccsp_memory.h"
 #include <ccsp_custom.h>
+#include <ccsp_alias_mgr.h>
+#include <ccsp_alias_mgr_helper.h>
 #include <dslh_definitions_database.h>
 #include <sys/ucontext.h>
 #include "ansc_platform.h"
@@ -57,6 +59,8 @@
 #if !defined(CCSP_INC_no_asm_sigcontext_h)
     #include <asm/sigcontext.h>
 #endif
+
+#define ALIAS_MANAGER_MAPPER_FILE "/usr/ccsp/custom_mapper.xml"
 
 #ifndef SAFEC_DUMMY_API
 #include "safe_str_lib.h"
@@ -89,6 +93,8 @@ static inline int strcmp_s(const char *dst, int dmax, const char *src, int *r) {
 #ifdef CCSP_MESSAGE_BUS_TEST
 
 static void *bus_handle = NULL;
+
+static ANSC_HANDLE aliasMgr = NULL; // AliasManager handle for DataModel names aliasing
 
 static char dst_pathname_cr[64] =  {0};
 static char subsystem_prefix[32] = "";
@@ -821,6 +827,8 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
     errno_t rc  = -1;
     int     ind = -1;
     
+    char *interNameForNS = NULL;
+    char *internalNames[BUSCLIENT_MAX_COUNT_SUPPORTED] = {NULL};
    
     //CCSP_Msg_SleepInMilliSeconds(500);
 
@@ -840,6 +848,15 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
         else
         {
             gettimeofday(&start, NULL);
+
+            if (aliasMgr != NULL)
+            {
+                interNameForNS = CcspAliasMgrGetFirstInternalName(aliasMgr, pInputCmd[0].result[0].pathname);
+                if (interNameForNS)
+                {
+                    pInputCmd[0].result[0].pathname = interNameForNS;
+                }
+            }
 
             ret = CcspBaseIf_discComponentSupportingNamespace 
                 (
@@ -1034,6 +1051,19 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
             {           
                 val[i].parameterName  = AnscCloneString(pInputCmd->result[i].pathname);
 
+                if (aliasMgr != NULL)
+                {
+                    internalNames[i] = CcspAliasMgrGetFirstInternalName(aliasMgr, val[i].parameterName);
+                    if (internalNames[i])
+                    {
+                      if (val[i].parameterName)
+                      {
+                          AnscFreeMemory(val[i].parameterName);
+                      }
+                      val[i].parameterName = internalNames[i];
+                    }
+                }
+
                 runSteps = __LINE__;
                 rc = strcmp_s("void", strlen("void"), pInputCmd->result[i].val2, &ind);
                 if(rc != EOK)
@@ -1220,6 +1250,20 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
             while ( pInputCmd->result[size].pathname )
             {
                 valAttr[size].parameterName  = AnscCloneString(pInputCmd->result[size].pathname);
+
+                if (aliasMgr != NULL)
+                {
+                    internalNames[i] = CcspAliasMgrGetFirstInternalName(aliasMgr, valAttr[size].parameterName);
+                    if (internalNames[i])
+                    {
+                      if (valAttr[size].parameterName)
+                      {
+                        AnscFreeMemory(valAttr[size].parameterName);
+                      }
+                      valAttr[size].parameterName = internalNames[i];
+                    }
+                }
+
                 if ( pInputCmd->result[size].val1 )
                 {
                     valAttr[size].notificationChanged = 1;
@@ -1352,6 +1396,16 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
             while ( pInputCmd->result[i].pathname )
             {
                 parameterNames[i] = pInputCmd->result[i].pathname;
+
+                if (aliasMgr != NULL)
+                {
+                    internalNames[i] = CcspAliasMgrGetFirstInternalName(aliasMgr, parameterNames[i]);
+                    if (internalNames[i])
+                    {
+                      parameterNames[i] = internalNames[i];
+                    }
+                }
+
                 i++;
             }
 
@@ -1435,6 +1489,16 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
             while ( pInputCmd->result[i].pathname )
             {
                 parameterNames[i] = pInputCmd->result[i].pathname;
+
+                if (aliasMgr != NULL)
+                {
+                  internalNames[i] = CcspAliasMgrGetFirstInternalName(aliasMgr, parameterNames[i]);
+                  if (internalNames[i])
+                  {
+                     parameterNames[i] = internalNames[i];
+                  }
+                }
+
                 i++;
             }
             gettimeofday(&start, NULL);
@@ -1571,6 +1635,16 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
             while ( pInputCmd->result[i].pathname )
             {
                 parameterNames[i] = pInputCmd->result[i].pathname;
+
+                if (aliasMgr != NULL)
+                {
+                    internalNames[i] = CcspAliasMgrGetFirstInternalName(aliasMgr, parameterNames[i]);
+                    if (internalNames[i])
+                    {
+                      parameterNames[i] = internalNames[i];
+                    }
+                }
+
                 i++;
             }
 
@@ -1682,6 +1756,21 @@ static int apply_cmd(PCMD_CONTENT pInputCmd )
 
         printf("\n"color_end);
         
+    }
+
+    if (interNameForNS)
+    {
+        AnscFreeMemory(interNameForNS);
+        interNameForNS = NULL;
+    }
+
+    for (i = 0; i < BUSCLIENT_MAX_COUNT_SUPPORTED; i++)
+    {
+        if (internalNames[i])
+        {
+            AnscFreeMemory(internalNames[i]);
+            internalNames[i] = NULL;
+        }
     }
     
     while( size2 && ppComponents)
@@ -2116,6 +2205,26 @@ static void signal_interrupt (int i)
             print_help();
 
 
+/**************************************************************************/
+/*! \fn bool is_customer_data_model()
+ **************************************************************************
+ *  \brief Check whether customer data-model is in place
+ *  \return true if the customer data-model is enabled
+ **************************************************************************/
+static bool is_customer_data_model (void)
+{
+    char sysbuf[8];
+
+    syscfg_get (NULL, "custom_data_model_enabled", sysbuf, sizeof(sysbuf));
+
+    if (strcmp (sysbuf, "1") == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
 int main (int argc, char *argv[])
 {
 //    void *bus_handle2;
@@ -2307,6 +2416,19 @@ int main (int argc, char *argv[])
         return -1;
     }
 
+    //Load AliasManager
+    if (is_customer_data_model())
+    {
+        aliasMgr = CcspAliasMgrInitialize();
+
+        if (!CcspAliasMgrLoadMappingFile(aliasMgr, ALIAS_MANAGER_MAPPER_FILE))
+        {
+            printf("dmcli: Failed to load alias mapping file %s\n", ALIAS_MANAGER_MAPPER_FILE);
+            CcspAliasMgrFree(aliasMgr);
+            aliasMgr = NULL;
+        }
+    }
+
     runSteps = __LINE__;
 
     while( 1 )
@@ -2409,6 +2531,11 @@ int main (int argc, char *argv[])
     // exit     
     CCSP_Message_Bus_Exit(bus_handle);
     //printf("count %d %d  \n", count, errcount );
+
+    if (aliasMgr)
+    {
+        CcspAliasMgrFree(aliasMgr);
+    }
 
     runSteps = __LINE__;
     
